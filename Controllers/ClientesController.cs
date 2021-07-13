44fa83c4c69ea5fd2c71e070fdd3531c3e3735f8
wilsesa <wilson.sanchez14@hotmail.com>
2021-07-13 09:18:00 -0300

    Projeto demo API Web

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,36 @@
+﻿using DemoWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DemoWebAPI.Controllers
+{
+    public class ClientesController : ApiController
+    {
+        private static List<Cliente> clientes = new List<Cliente>();
+
+        //1 metodo do API
+        public List<Cliente> Get()
+        {
+            return clientes;
+        }
+
+        public void Post(string nome)
+        {
+            if (!string.IsNullOrEmpty(nome))
+            {
+                clientes.Add(new Cliente(nome));
+            }
+            
+        }
+
+        public void Delete(string nome)
+        {
+            //utilizamos linq com expressao lambida
+            clientes.RemoveAt(clientes.IndexOf(clientes.First(x => x.Nome.Equals(nome))));
+        }
+    }
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoWebAPI.Models
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+
+        public Cliente(string texto)
+        {
+            this.Nome = texto;
+        }
+    }
+}
